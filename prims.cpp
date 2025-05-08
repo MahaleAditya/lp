f79ed76b0ea729b34f03987e608778abeadf805a
MahaleAditya <153471259+MahaleAditya@users.noreply.github.com>
2025-05-08 07:01:23 +0530

    prims.cpp

--- prims.cpp
+++ prims.cpp
@@ -3,32 +3,46 @@
 #include <climits>
 using namespace std;
 
-int minKey(vector<int> key, vector<bool> mstSet, int V) {
-    int min = INT_MAX, min_index;
-    for(int v = 0; v < V; v++)
-        if(!mstSet[v] && key[v] < min)
-            min = key[v], min_index = v;
+// Function to find the vertex with the minimum key value not yet included in MST
+int minKey(const vector<int>& key, const vector<bool>& mstSet, int V) {
+    int min = INT_MAX, min_index = -1;
+    for(int v = 0; v < V; v++) {
+        if(!mstSet[v] && key[v] < min) {
+            min = key[v];
+            min_index = v;
+        }
+    }
     return min_index;
 }
 
-void primMST(vector<vector<int>> graph, int V) {
-    vector<int> key(V, INT_MAX), parent(V, -1);
-    vector<bool> mstSet(V, false);
+// Function to implement Prim's MST algorithm
+void primMST(const vector<vector<int>>& graph, int V) {
+    vector<int> key(V, INT_MAX);      // Stores minimum weights
+    vector<int> parent(V, -1);        // Stores MST structure
+    vector<bool> mstSet(V, false);    // True if vertex is included in MST
 
-    key[0] = 0;
+    key[0] = 0;  // Start from vertex 0
 
     for(int count = 0; count < V - 1; count++) {
         int u = minKey(key, mstSet, V);
         mstSet[u] = true;
 
-        for(int v = 0; v < V; v++)
-            if(graph[u][v] && !mstSet[v] && graph[u][v] < key[v])
-                parent[v] = u, key[v] = graph[u][v];
+        for(int v = 0; v < V; v++) {
+            if(graph[u][v] && !mstSet[v] && graph[u][v] < key[v]) {
+                parent[v] = u;
+                key[v] = graph[u][v];
+            }
+        }
     }
 
-    cout << "Edge \tWeight\n";
-    for(int i = 1; i < V; i++)
+    int totalWeight = 0;
+    cout << "\nEdge \tWeight\n";
+    for(int i = 1; i < V; i++) {
         cout << parent[i] << " - " << i << "\t" << graph[i][parent[i]] << "\n";
+        totalWeight += graph[i][parent[i]];
+    }
+
+    cout << "Total weight of MST: " << totalWeight << endl;
 }
 
 int main() {
@@ -43,9 +57,9 @@ int main() {
             cin >> graph[i][j];
 
     primMST(graph, V);
+
+    return 0;
 }
-
-
 //ip
 /*
 Enter number of vertices: 5
